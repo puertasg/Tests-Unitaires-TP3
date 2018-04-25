34313ce237f76d44e6a3356acab91102f5d80818
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-25 10:52:02 +0200

    Déclaration calc dans setUp avec @Before

--- src/test/java/dev/service/CalculServiceTest.java
+++ src/test/java/dev/service/CalculServiceTest.java
@@ -2,6 +2,7 @@ package dev.service;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +12,17 @@ import dev.exception.CalculException;
 public class CalculServiceTest {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(CalculService.class);
+	private CalculService calc;
+	@Before
+	public void setUp()
+	{
+		CalculService calc = new CalculService();
+	}
 	
 	@Test
 	public void testAdditionner() throws Exception
 	{
 		LOG.debug("Etant donné, une instance de la classe CalculService");
-		CalculService calc = new CalculService();
-		
 		LOG.debug("Lorsque j'évalue l'addition de l'expression 1+3+4");
 		int somme = calc.additionner("1+3+4");
 		
@@ -29,8 +34,6 @@ public class CalculServiceTest {
 	public void testInvalidAdditionner()
 	{
 		LOG.debug("Etant donné, une instance de la classe CalculService");
-		CalculService calc = new CalculService();
-		
 		LOG.debug("Lorsque j'évalue l'addition de l'expression invalide 1+3+a");
 		int somme = calc.additionner("1+3+a");
 		
