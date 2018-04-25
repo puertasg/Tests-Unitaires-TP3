1c31efe393426a092af1b68a9055cd5458759ea6
Gauthier Puertas <gauthier.puertas@gmail.com>
2018-04-25 10:11:37 +0200

    Ajout CalculServiceTest, implémentation logger, ajout test unitaire

---
+++ src/test/java/dev/service/CalculServiceTest.java
@@ -0,0 +1,25 @@
+package dev.service;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalculServiceTest {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CalculService.class);
+	
+	@Test
+	public void testAdditionner() throws Exception
+	{
+		LOG.info("Etant donné, une instance de la classe CalculService");
+		CalculService calc = new CalculService();
+		
+		LOG.info("Lorsque j'évalue l'addition de l'expression 1+3+4");
+		int somme = calc.additionner("1+3+4");
+		
+		LOG.info("Alors j'obtiens le résultat 8");
+		assertEquals(8, somme);
+	}
+}
